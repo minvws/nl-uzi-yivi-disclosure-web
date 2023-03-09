dd770fe2571d637e6cbebddb09d8376c0a99c4b4
Gerbrand Bosch <gerbrandb@gmail.com>
2023-03-09 15:15:16 +0100

    removed unused import

--- app/Providers/UziServiceProvider.php
+++ app/Providers/UziServiceProvider.php
@@ -7,8 +7,6 @@ namespace App\Providers;
 use App\Http\Controllers\IrmaController;
 use App\Services\Uzi\UziInterface;
 use App\Services\Uzi\UziJweDecryptService;
-use App\Services\Uzi\UziLoginCallbackHandler;
-use App\Services\Uzi\UziLoginCallbackHandlerInterface;
 use App\Services\Uzi\UziService;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,7 +14,6 @@ class UziServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->app->singleton(UziLoginCallbackHandlerInterface::class, UziLoginCallbackHandler::class);
         $this->app->singleton(
             UziInterface::class,
             function () {
