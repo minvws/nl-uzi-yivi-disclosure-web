bb3cf6451c04f5d1f62e06aa0e11a74b5cad08a8
Gerbrand Bosch <gerbrandb@gmail.com>
2023-05-04 09:23:38 +0200

    removed unused import

--- app/Http/Controllers/Auth/UziAuthController.php
+++ app/Http/Controllers/Auth/UziAuthController.php
@@ -8,7 +8,6 @@ use App\Services\Uzi\UziInterface;
 use Exception;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class UziAuthController
 {
