373077a98ddc6ad63316878ae0a179ffd16dcef2
Rick Lambrechts <rick@rl-webdiensten.nl>
2023-06-19 12:31:29 +0200

    feat: use privacybydesign/yivi-frontend package (#41)
    * feat: use @privacybydesign/yivi-frontend package
    
    * Update resources/css/external-component.css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,101 @@
 import './bootstrap';
 
 import '@minvws/manon/collapsible';
+import { onDomReady } from '@minvws/manon/utils';
+import * as Yivi from '@privacybydesign/yivi-frontend';
+
+onDomReady(initYiviButtons);
+
+function initYiviButtons()
+{
+    // For each yivi start button
+    document.querySelectorAll('[data-yivi-start-button]')
+        .forEach(function (button) {
+            initYiviButton(button);
+        });
+}
+
+/**
+ * Initialize a yivi start button with click event listener
+ * @param {Element} button
+ */
+function initYiviButton(button)
+{
+    // Get the data attributes
+    const ura = button.getAttribute('data-yivi-ura');
+    const title = button.getAttribute('data-yivi-title');
+    const csrfToken = button.getAttribute('data-csrf-token');
+
+    if (isEmpty(ura)) {
+        console.error("Missing data-yivi-ura attribute on button");
+        return;
+    }
+
+    if (isEmpty(title)) {
+        console.error("Missing data-yivi-title attribute on button");
+        return;
+    }
+
+    if (isEmpty(csrfToken)) {
+        console.error("Missing data-csrf-token attribute on button");
+        return;
+    }
+
+    // Add a click event listener
+    button.addEventListener('click', function () {
+        // Start the yivi session
+        yiviStart(ura, title, csrfToken);
+    });
+}
+
+/**
+ * Start a yivi session
+ * @param {string} ura
+ * @param {string} title
+ * @param {string} csrfToken
+ */
+function yiviStart(ura, title, csrfToken)
+{
+    let options = {
+        // Developer options
+        debugging: true,
+
+        // Front-end options
+        language:  'en',
+        translations: {
+            header: title,
+            loading: 'Just one second please!'
+        },
+
+        // Back-end options
+        session: {
+            url: '/irma',
+            start: {
+                url: o => `${o.url}/start`,
+                method: 'POST',
+                headers: {
+                    "X-CSRF-TOKEN": csrfToken,
+                    "Content-Type": 'application/x-www-form-urlencoded;charset=UTF-8'
+                },
+                body: "ura=" + ura
+            },
+            result: false
+        }
+    };
+
+    const yiviPopup = Yivi.newPopup({
+        ...options,
+        element: '#yivi-web-form'
+    });
+    yiviPopup.start();
+}
+
+/**
+ * Check if the given data is empty
+ * @param {string|undefined|null} data
+ * @returns {boolean}
+ */
+function isEmpty(data)
+{
+    return data === "" || data === null || data === undefined;
+}
--- resources/views/disclosure.blade.php
+++ resources/views/disclosure.blade.php
@@ -12,38 +12,32 @@
     <div>
         <h1>hier disclosures</h1>
         <p>Veel text</p>
-        <div id="irma-web-form" class="external-component"></div>
+        <div id="yivi-web-form" class="external-component"></div>
 
         <div class="horizontal-scroll">
-          <table>
-            <thead>
-              <tr>
-                <th scope="col">Instantie</th>
-                <th scope="col">Rollen</th>
-                <th scope="col">Inladen IRMA</th>
-              </tr>
-            </thead>
-            <tbody>
-            @foreach (Auth::user()->uras as $ura)
-              <tr>
-                <td>{{ $ura->entityName }} ({{ $ura->ura }})</td>
-                <td>@foreach ($ura->roles as $role){{ $role }}<br> @endforeach</td>
-                <td>
-                  <div>
-                        @csrf
-                        <input type="button" value="Inladen" onclick="irmaStart({{ $ura->ura }}, 'Scan QR code om uw rollen te laden voor {{ $ura->entityName }}')"></button>
-                  </div>
-                </td>
-              </tr>
-            @endforeach
-            </tbody>
-          </table>
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">Instantie</th>
+                        <th scope="col">Rollen</th>
+                        <th scope="col">Inladen IRMA</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach (Auth::user()->uras as $ura)
+                    <tr>
+                        <td>{{ $ura->entityName }} ({{ $ura->ura }})</td>
+                        <td>@foreach ($ura->roles as $role){{ $role }}<br> @endforeach</td>
+                        <td>
+                            <div>
+                                <button data-yivi-start-button data-csrf-token="{{ csrf_token() }}" data-yivi-ura="{{ $ura->ura }}" data-yivi-title="Scan QR code om uw rollen te laden voor {{ $ura->entityName }}" type="button">Inladen</button>
+                            </div>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </section>
-@push('scripts')
-<script type="text/javascript" src="irmajs-v0.4.2/irma.js"></script>
-<script type="text/javascript" src="js/disclosure.js"></script>
-<link rel="stylesheet" href="irmajs-v0.4.2/irma.css">
-@endpush
 @endsection
