56251c49c2200f650b154346b6c45d381554f8dd
Rick Lambrechts <rick@rl-webdiensten.nl>
2025-08-26 16:05:00 +0200

    Fix config key for yivi authentication private key

--- app/Services/Yivi/YiviSessionService.php
+++ app/Services/Yivi/YiviSessionService.php
@@ -37,7 +37,7 @@ class YiviSessionService
         protected bool $authenticationEnabled,
         #[Config('yivi.authentication.jwt_issuer')]
         protected string $authenticationJwtIssuer = '',
-        #[JWKFromConfig('yivi.authentication_private_key_path')]
+        #[JWKFromConfig('yivi.authentication.jwt_private_key_path')]
         protected ?JWK $authenticationJwtPrivateKey = null,
     ) {
         if ($this->authenticationEnabled && !$this->authenticationJwtPrivateKey) {
